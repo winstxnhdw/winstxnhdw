5ea8034ac8e6540e9570556e479ffab2d0cf41a9
winstxnhdw <winstxnhdw@gmail.com>
2021-12-23 01:42:54 +0800

    update

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -6,5 +6,8 @@ set -x SUDO_EDITOR nvim
 
 set -x PF_INFO "ascii title os wm editor shell uptime pkgs memory palette"
 
+eval /home/winston/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+
 pfetch
 starship init fish | source
+
