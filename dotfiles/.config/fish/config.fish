3d31dcd055b5a7ae63bb2917d7fa89f2734d0d68
winstxnhdw <winstxnhdw@gmail.com>
2022-01-03 09:36:32 +0800

    auto-commit

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -6,6 +6,7 @@ set -x SUDO_EDITOR nvim
 
 set -x PF_INFO "ascii title os wm editor shell uptime pkgs memory palette"
 
+set -x _CONDA_ROOT "/home/winston/miniconda3"
 eval /home/winston/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 
 pfetch
---
+++ dotfiles/.config/fish/functions/conda-forge.fish
@@ -0,0 +1,3 @@
+function conda-forge --description 'alias conda-forge=conda install -c conda-forge'
+  conda install -c conda-forge $argv; 
+end
