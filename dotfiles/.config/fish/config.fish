3d624a84cd5ee50fdd78face72e3cfaa197442b6
winstxnhdw <winstxnhdw@gmail.com>
2022-01-27 21:06:51 +0800

    auto-commit

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,4 +1,7 @@
 set fish_greeting
+pfetch
+
+set fish_user_paths $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin $fish_user_paths
 
 set -x EDITOR nvim
 set -x VISUAL nvim
@@ -9,6 +12,6 @@ set -x PF_INFO "ascii title os wm editor shell uptime pkgs memory palette"
 set -x _CONDA_ROOT "/home/winston/miniconda3"
 eval /home/winston/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 
-pfetch
+conda activate default
 starship init fish | source
 
