1de38b97ce4f19e8067c9ba44dcc7b5570f3b9bd
winstxnhdw <winstxnhdw@gmail.com>
2022-02-28 11:03:48 +0800

    add bang bang function

--- dotfiles/.config/fish/functions/notif.fish
+++ dotfiles/.config/fish/functions/notif.fish
@@ -1,14 +1,14 @@
 function notif --description 'Toggles Dunst notifications'
   switch $argv
-    case 'on'
+    case "on"
       echo "Dunst notifications has been turned on."
       command dunstctl set-paused false
     
-    case 'off'
+    case "off"
       command dunstctl set-paused true
       echo "Dunst notifications has been turned off."
     
-    case '*'
+    case "*"
       echo "Usage: notif [on|off]"
       return
   end
---
+++ dotfiles/.config/fish/functions/paste_previous_arguments.fish
@@ -0,0 +1,10 @@
+function paste_previous_arguments
+  switch (commandline -t)
+    case '!'
+      commandline -t ''
+      commandline -f history-token-search-backward
+      
+    case '*'
+      commandline -i '$'
+  end
+end
