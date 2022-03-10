ff5021c0b2c2f26ab37c40bfeef1208003ded47b
winstxnhdw <winstxnhdw@gmail.com>
2022-03-11 00:47:50 +0800

    handle multiple dirs or files

--- dotfiles/.config/fish/functions/rm.fish
+++ dotfiles/.config/fish/functions/rm.fish
@@ -1,29 +1,31 @@
 function rm --description 'alias rm=rm'
-  set -l remove (command rm $argv 2>&1)
+  for arg in $argv
+    set -l remove (command rm $arg 2>&1)
 
-  if test -z "$remove"
-    return
-    
-  else
-    for line in $remove
-      echo $line
-    end
-
-    switch $argv
-      case '--*'
-        return
-    end
-  end
-  
-  switch $remove
-    case '*Is a directory'
-      echo 'Would you like to use rm -r instead? [y/N] '
-      switch (read -t)
-        case Y y
-          echo "Attempting to remove directory."
-          command rm -r $argv
-        case '' N n
-          echo "No directories will be removed."
+    if test -z "$remove"
+      return
+      
+    else
+      for line in $remove
+        echo $line
       end
+
+      switch $arg
+        case '--*'
+          return
+      end
+    end
+    
+    switch $remove
+      case '*Is a directory'
+        echo 'Would you like to use rm -r instead? [y/N] '
+        switch (read -t)
+          case Y y
+            echo "Attempting to remove directory."
+            command rm -r $arg
+          case '' N n
+            echo "No directories will be removed."
+        end
+    end
   end
 end
