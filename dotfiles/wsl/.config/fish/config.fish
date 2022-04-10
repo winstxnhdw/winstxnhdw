001b4105cda182ca7137a0129549d720251ece0b
winstxnhdw <winstxnhdw@gmail.com>
2022-04-10 21:10:09 +0800

    update

--- dotfiles/wsl/.config/fish/config.fish
+++ dotfiles/wsl/.config/fish/config.fish
@@ -1,11 +1,9 @@
 set fish_greeting
 set fish_user_paths $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin $fish_user_paths
+set -x PF_INFO "ascii title os wm editor shell uptime pkgs memory palette"
 
 set -x EDITOR nvim
 set -x VISUAL nvim
 set -x SUDO_EDITOR nvim
 
-set -x PF_INFO "ascii title os wm editor shell uptime pkgs memory palette"
-
 starship init fish | source
-pfetch
