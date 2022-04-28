3a88744247e5b1336598d8a8a4beb9cb09c14d45
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-04-29 04:16:06 +0800

    Delete edit-grub.fish

--- dotfiles/wsl/.config/fish/functions/edit-grub.fish
+++
@@ -1,3 +0,0 @@
-function edit-grub --wraps='xdg-open (readlink -e /etc/default/grub)' --description 'alias edit-grub=xdg-open (readlink -e /etc/default/grub)'
-  xdg-open (readlink -e /etc/default/grub)
-end
