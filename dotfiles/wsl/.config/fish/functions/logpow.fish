70d526cf9461711feece8fd9fb66ace97113962b
winstxnhdw <winstxnhdw@gmail.com>
2022-07-30 03:28:55 +0800

    change from 4 to 2 space

--- dotfiles/wsl/.config/fish/functions/logpow.fish
+++ dotfiles/wsl/.config/fish/functions/logpow.fish
@@ -1,20 +1,20 @@
 function logpow --description 'Convert from exponential form to logarithmic form'
-    set -f argv_joined (string join '' $argv)
-    
-    switch $argv_joined
-        case '*=*'
-            for side in (string split = $argv_joined)
-                switch $side
-                    case '*^*'
-                        set -l exponentiation (string split ^ $side)
-                        set -f base $exponentiation[1]
-                        set -f exponent $exponentiation[2]
+  set -f argv_joined (string join '' $argv)
+  
+  switch $argv_joined
+    case '*=*'
+      for side in (string split = $argv_joined)
+        switch $side
+          case '*^*'
+            set -l exponentiation (string split ^ $side)
+            set -f base $exponentiation[1]
+            set -f exponent $exponentiation[2]
 
-                    case '*'
-                        set -f result $side
-                end
-            end
+          case '*'
+            set -f result $side
+        end
+      end
 
-            echo log_\{$base\}\($exponent\) = $result
-    end
+      echo log_\{$base\}\($exponent\) = $result
+  end
 end
