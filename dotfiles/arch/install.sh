15d425833dbb74d9107ad8c5c42d579a3f5c3fe7
winstxnhdw <winstxnhdw@gmail.com>
2022-11-02 03:43:58 +0800

    update install.sh

--- dotfiles/arch/install.sh
+++ dotfiles/arch/install.sh
@@ -9,7 +9,8 @@ then
     exit 1
 fi
 
-origin_path=~/projects/winstxnhdw/dotfiles/arch/.config
+origin_path=~/projects/winstxnhdw/dotfiles/arch/
+config_path=$origin_path/.config
 target_path=~/.config
 
 for directory in `ls -p .config | grep / | sed 's@/@@g'`
@@ -17,7 +18,7 @@ do
     # Create symbolic link
     echo "Attempting to create a symbolic link for $directory in $target_path/$directory"
     rm -r $target_path/$directory
-    ln -sf $origin_path/$directory $target_path
+    ln -sf $config_path/$directory $target_path
 done
 
 for file in `ls -p .config | grep -v /`
@@ -25,10 +26,14 @@ do
     # Create symbolic link
     echo "Attempting to create a symbolic link for $file in $target_path/$file"
     rm $target_path/$file
-    ln -sf $origin_path/$file $target_path
+    ln -sf $config_path/$file $target_path
 done
 
-# Create symbolic link for cron
-cron_path=$origin_path/../cron/cron
+# Create symbolic link for fonts
+echo "Attempting to create a symbolic link for fonts"
+ln -sf $origin_path/fonts ~/.local/share/fonts
+
+# Setting crontab
+cron_path=$origin_path/cron/cron
 echo "Attempting to set $cron_path as crontab"
 crontab $cron_path
