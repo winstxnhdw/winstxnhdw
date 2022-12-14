ea99c59472590fd71187d8bb55090c6c615d3644
winstxnhdw <winstxnhdw@gmail.com>
2022-12-14 10:05:37 +0800

    update

--- dotfiles/arch/.config/i3/scripts/power.sh
+++
@@ -1,9 +0,0 @@
-chosen=$(echo -e "Logout [L]\nShutdown [S]\nReboot [R]" | dmenu)
-
-if [[ $chosen = "Logout" ]]; then
-	pkill X
-elif [[ $chosen = "Shutdown" ]]; then
-	systemctl poweroff
-elif [[ $chosen = "Reboot" ]]; then
-	systemctl reboot
-fi
