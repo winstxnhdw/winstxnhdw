518f4fe34c9c70301b4cd62bbe9aad3444bbfe65
winstxnhdw <winstxnhdw@gmail.com>
2023-02-05 18:16:52 +0800

    fix pull dotfiles

--- dotfiles/arch/.config/fish/functions/pull-dotfiles.fish
+++ dotfiles/arch/.config/fish/functions/pull-dotfiles.fish
@@ -1,3 +1,3 @@
-function pull-dotfiles --wraps='git pull ~/projects/winstxnhdw' --description 'alias pull-dotfiles=git pull ~/projects/winstxnhdw'
-  git pull ~/projects/winstxnhdw $argv; 
+function pull-dotfiles --wraps='git -C ~/projects/winstxnhdw pull' --description 'alias pull-dotfiles=git -C ~/projects/winstxnhdw pull'
+  git -C ~/projects/winstxnhdw pull
 end
