574ec50d0882c2a9f727366df34481c35334bfce
winstxnhdw <winstxnhdw@gmail.com>
2023-02-07 23:23:50 +0800

    more alias

---
+++ dotfiles/arch/.config/fish/functions/github.fish
@@ -0,0 +1,11 @@
+function github --description='Goes to the repository of the specified project'
+  set -l github_url "https://github.com"
+
+  switch $argv[1]
+    case '-u'
+      open $github_url/$argv
+
+    case '*'
+      open $github_url/winstxnhdw/$argv
+  end
+end
