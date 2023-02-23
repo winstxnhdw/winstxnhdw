47c48786d155e3262ad472dd67d6ef2ea63755b7
winstxnhdw <winstxnhdw@gmail.com>
2023-02-24 02:00:52 +0800

    fix first-push

--- dotfiles/arch/.config/fish/functions/first-push.fish
+++ dotfiles/arch/.config/fish/functions/first-push.fish
@@ -1,5 +1,6 @@
 function first-push --description 'First push from a local repository to a git repository'
   git init
+  git branch -M main
   git add .
   git commit -m "chore: initialise repository"
   git remote add origin $argv
