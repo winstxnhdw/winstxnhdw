902de5422dcf72a8aab065c14251a916266e1109
winstxnhdw <winstxnhdw@gmail.com>
2023-05-02 21:55:32 +0800

    update

--- dotfiles/arch/.config/fish/functions/firefox.fish
+++ dotfiles/arch/.config/fish/functions/firefox.fish
@@ -1,3 +1,3 @@
 function firefox --wraps=firefox-developer-edition --description 'alias firefox=firefox-developer-edition'
-  firefox-developer-edition $argv; 
+  firefox-developer-edition $argv
 end
--- dotfiles/arch/.config/polybar/scripts/get_networks.sh
+++ dotfiles/arch/.config/polybar/scripts/get_networks.sh
@@ -1,7 +1,7 @@
 while true
 do
     clear
-    iwctl station wlan0 scan
+    iwctl station wlan0 scan > /dev/null
     iwctl station wlan0 get-networks
     sleep 3
 done
