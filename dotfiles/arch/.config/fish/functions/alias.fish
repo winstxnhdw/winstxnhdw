33d206d40ec37ae9766328677ddfefa21f34c9cb
winstxnhdw <winstxnhdw@gmail.com>
2023-05-25 12:29:03 +0800

    feat: add `alias` to edit or print alias contents

---
+++ dotfiles/arch/.config/fish/functions/alias.fish
@@ -0,0 +1,11 @@
+function alias --description "Print/edit fish alias"
+  set -l alias_file $HOME/.config/fish/functions/$argv[1].fish
+
+  switch $argv[2]
+    case '--edit' '-e'
+      vim $alias_file
+
+    case '*'
+      cat $alias_file
+  end
+end
--- dotfiles/arch/.config/fish/functions/clear-trash.fish
+++ dotfiles/arch/.config/fish/functions/clear-trash.fish
@@ -1,3 +1,3 @@
 function clear-trash --wraps='rm -r ~/.local/share/Trash/files/*' --description 'alias clear-trash=rm -r ~/.local/share/Trash/files/*'
-  command rm -r ~/.local/share/Trash/files/* $argv;
+  rm -rf ~/.local/share/Trash/files/* $argv;
 end
