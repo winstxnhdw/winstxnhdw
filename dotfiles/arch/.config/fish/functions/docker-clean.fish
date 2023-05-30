91366d5791526b0dab4bd83b0a793e1ca9237779
winstxnhdw <winstxnhdw@gmail.com>
2023-05-30 09:55:21 +0800

    feat: add `docker-clean` alias

---
+++ dotfiles/arch/.config/fish/functions/docker-clean.fish
@@ -0,0 +1,3 @@
+function docker-clean --wraps='docker system prune -a -f' --description 'alias docker-clean=docker system prune -a -f'
+  docker system prune -a -f
+end
--- dotfiles/arch/.config/fish/functions/docker-stop.fish
+++ dotfiles/arch/.config/fish/functions/docker-stop.fish
@@ -1,4 +1,3 @@
 function docker-stop --wraps='docker stop (docker ps -aq)' --description 'alias docker-stop=docker stop (docker ps -aq)'
   docker stop (docker ps -aq) $argv
-        
 end
