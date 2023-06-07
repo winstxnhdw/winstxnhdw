eeab83081bb4bc4f0c9258c24b1b3448c12dac2f
winstxnhdw <winstxnhdw@gmail.com>
2023-06-08 00:19:35 +0800

    feat: only use `ncdu` when `-v` flag is passed

--- dotfiles/arch/.config/fish/functions/file-usage.fish
+++ dotfiles/arch/.config/fish/functions/file-usage.fish
@@ -1,3 +1,9 @@
 function file-usage --wraps='ncdu -x --exclude Win10/' --wraps='ncdu -x / --exclude Win10/' --description 'alias file-usage=ncdu -x / --exclude Win10/'
-  ncdu -x / --exclude Win10/ $argv; 
+  switch $argv[1]
+    case '-v' '--verbose'
+      ncdu -x / --exclude Win10/ $argv[2..-1]
+
+    case '*'
+      df -h /
+  end
 end
