2c66833b9368720119feb1b7add5d53b63594d32
winstxnhdw <winstxnhdw@gmail.com>
2023-07-09 22:25:45 +0800

    chore: use `open` instead of `xdg-open`

--- dotfiles/arch/.config/fish/functions/edit-cron.fish
+++ dotfiles/arch/.config/fish/functions/edit-cron.fish
@@ -1,3 +1,3 @@
-function edit-cron --wraps='xdg-open (readlink -e /var/spool/cron/$USER)' --description 'alias edit-cron=xdg-open (readlink -e /var/spool/cron/$USER)'
-  xdg-open (readlink -e /var/spool/cron/$USER)
+function edit-cron --wraps='open (readlink -e /var/spool/cron/$USER)' --description 'alias edit-cron=open (readlink -e /var/spool/cron/$USER)'
+  open (readlink -e /var/spool/cron/$USER)
 end
