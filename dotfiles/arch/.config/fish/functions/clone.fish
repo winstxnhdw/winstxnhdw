8551de616fceb951085ded230d045aade7858c1c
winstxnhdw <winstxnhdw@gmail.com>
2023-08-12 22:13:44 +0800

    fix: only split once

--- dotfiles/arch/.config/fish/functions/clone.fish
+++ dotfiles/arch/.config/fish/functions/clone.fish
@@ -2,7 +2,7 @@ function clone --description 'Switch to the git directory after cloning'
   switch $argv
     case '*.git'
       git clone --recursive $argv
-      pushd (string split . (basename $argv))[1]
+      pushd (string split -r -m 1 . (basename $argv))[1]
 
     case '*'
       git clone --recursive git@github.com:winstxnhdw/$argv.git
