5a873609d620f5515a847e4e11966282c2dda2d2
winstxnhdw <winstxnhdw@gmail.com>
2023-11-22 23:25:38 +0800

    feat: sort by most recent

--- dotfiles/arch/.config/fish/completions/projects.fish
+++ dotfiles/arch/.config/fish/completions/projects.fish
@@ -1,2 +1,2 @@
-complete -c projects -a "(exa $HOME/projects/)"
+complete -c projects -k -a "(exa --sort modified --reverse --no-time --no-user --no-permissions --no-filesize $HOME/projects/)"
 
--- dotfiles/arch/.config/fish/completions/vsc.fish
+++ dotfiles/arch/.config/fish/completions/vsc.fish
@@ -1,2 +1,2 @@
-complete -c vsc -a "(exa $HOME/projects/)"
+complete -c vsc -k -a "(exa --sort modified --reverse --no-time --no-user --no-permissions --no-filesize $HOME/projects/)"
 
