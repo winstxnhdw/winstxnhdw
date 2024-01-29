6225918cbba207de9b5d2f6056d2adda77e3bb2d
winstxnhdw <winstxnhdw@gmail.com>
2024-01-29 21:54:44 +0800

    fix: reset okularpartrc

--- dotfiles/arch/.config/nvim/after/plugin/comment.lua
+++ dotfiles/arch/.config/nvim/after/plugin/comment.lua
@@ -1,4 +1,1 @@
-require('nvim_comment').setup({
-    comment_empty = false,
-    comment_empty_trim_whitespace = false
-})
+require("Comment").setup()
--- dotfiles/arch/.config/nvim/lua/remap.lua
+++ dotfiles/arch/.config/nvim/lua/remap.lua
@@ -1,4 +1,10 @@
-local nnoremap = require('keymap').nnoremap
+local nnoremap = require("keymap").nnoremap
+local telescope = require("telescope.builtin")
 
-nnoremap('<leader>pv', '<cmd>Ex<CR>')
-nnoremap('<C-a>', 'gg<S-v>G')
+nnoremap("<leader>pv", "<cmd>Ex<CR>")
+nnoremap("<C-a>", "gg<S-v>G")
+
+nnoremap("<leader>ff", telescope.find_files)
+nnoremap("<leader>fg", telescope.live_grep)
+nnoremap("<leader>fb", telescope.buffers)
+nnoremap("<leader>fh", telescope.help_tags)
