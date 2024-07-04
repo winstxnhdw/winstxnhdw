b8b24bb44808ee96b8826e3def0231f7dc5c0e5f
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-07-04 10:09:34 +0100

    feat: make `cURL` silent

--- dotfiles/wsl/.config/fish/functions/translate.fish
+++ dotfiles/wsl/.config/fish/functions/translate.fish
@@ -1,5 +1,5 @@
 function translate --description 'Translates a string from one language to another'
   set -l text (string join %20 $argv[2..-1])
-  set -l source_language (curl "https://winstxnhdw-nllb-api.hf.space/api/v3/detect_language?text=$text" | jq -r .language)
-  curl "https://winstxnhdw-nllb-api.hf.space/api/v3/translate?text=$text&source=$source_language&target=$argv[1]" | jq -r .result
+  set -l source_language (curl -s "https://winstxnhdw-nllb-api.hf.space/api/v3/detect_language?text=$text" | jq -r .language)
+  curl -s "https://winstxnhdw-nllb-api.hf.space/api/v3/translate?text=$text&source=$source_language&target=$argv[1]" | jq -r .result
 end
