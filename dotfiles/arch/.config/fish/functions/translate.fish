ef8dbc2816ff2fdef1f13e9621bb407436f86a5c
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-07-04 10:04:31 +0100

    fix(translate): fix string split

--- dotfiles/arch/.config/fish/functions/translate.fish
+++ dotfiles/arch/.config/fish/functions/translate.fish
@@ -1,5 +1,5 @@
 function translate --description 'Translates a string from one language to another'
-  set -l text ($argv[2..-1] | string join %20)
-  set -l source_language (curl 'https://winstxnhdw-nllb-api.hf.space/api/v3/detect_language?text=$text' | jq -r .language)
-  curl 'https://winstxnhdw-nllb-api.hf.space/api/v3/translate?text=$text&source=$source_language&target=$argv[1]' | jq -r .result
+  set -l text (string join %20 $argv[2..-1])
+  set -l source_language (curl "https://winstxnhdw-nllb-api.hf.space/api/v3/detect_language?text=$text" | jq -r .language)
+  curl "https://winstxnhdw-nllb-api.hf.space/api/v3/translate?text=$text&source=$source_language&target=$argv[1]" | jq -r .result
 end
