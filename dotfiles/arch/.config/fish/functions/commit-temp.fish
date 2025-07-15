84b09e85eb3d0132e7f4f86720492c54f5f17ee8
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-07-15 18:34:07 +0100

    feat: use `fixup`

--- dotfiles/arch/.config/fish/functions/commit-temp.fish
+++ dotfiles/arch/.config/fish/functions/commit-temp.fish
@@ -1,5 +1,5 @@
 function commit-temp --description 'commit all changes (usually for squashing)'
   git add .
-  git commit -m "DO NOT PUSH. THIS IS A TEMPORARY COMMIT."
+  git commit --fixup=HEAD
   git rebase -i HEAD~2 || git reset --soft HEAD~1
 end
